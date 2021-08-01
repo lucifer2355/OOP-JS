89835e88bb9cf738cf38c0b022d4ddf1119186d7
lucifer2355 <dgajjar999@gmail.com>
2021-08-01 23:00:12 +0530

    add residents methods

--- index.js
+++ index.js
@@ -14,4 +14,23 @@ class House {
     this.price = price;
     this.residents = residents;
   }
+
+  getAddress = () => this.address;
+  getPrice = () => this.price;
+  getResidents = () => this.residents;
+
+  addResident = (resident) => {
+    this.residents.push(resident);
+  };
 }
+
+const Jon = new Person("Jon", 23);
+const David = new Person("David", 21);
+const Max = new Person("Max", 25);
+
+const house = new House("bhavnagar", 20000, [Jon, David, Max]);
+console.log(house.getResidents());
+
+const Harry = new Person("Harry", 19);
+house.addResident(Harry);
+console.log(house.getResidents());
